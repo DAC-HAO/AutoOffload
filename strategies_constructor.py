a4de0aedb32bde1102dbcb35c85de05bbd71819a
superhao1995 <804673818@qq.com>
2023-01-23 20:57:04 +0800

    test

--- strategies_constructor.py
+++ strategies_constructor.py
@@ -149,7 +149,7 @@ class OffloadStrategiesConstructor:
 
                 # if the node could free all dependencies in graph
                 # we could begin a new node
-                if _is_sink() or _is_param_comp_op():
+                if _is_sink():
                     region_list.append(region)
                     region = Region(r_id=region_id, nodes=[], param_indices=[])
                     region_id += 1
