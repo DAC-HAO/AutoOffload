89e939643bbcb89d7f3c1b1ab638bf6b033c5cfb
superhao1995 <804673818@qq.com>
2023-01-25 15:44:04 +0800

    test

--- strategies_constructor.py
+++ strategies_constructor.py
@@ -198,10 +198,11 @@ class OffloadStrategiesConstructor:
                     if n_par.op != "placeholder" and n_par.name in self.only_param_ops:
                         param_op_deps[n_par] -= 1
 
-                if len(region.nodes) != 0 and _maybe_param_comp_start():
+                if len(region.nodes) != 0 and act_n is not None and _maybe_param_comp_start():
 
+                    assert region.nodes.__contains__(act_n)
                     ns = []
-                    if region.nodes.__contains__(act_n) and act_n != region.nodes[-1]:
+                    if act_n != region.nodes[-1]:
                         border_n_idx = region.nodes.index(act_n)
                         ns = region.nodes[border_n_idx+1:]
                         region.nodes = region.nodes[:border_n_idx+1]
